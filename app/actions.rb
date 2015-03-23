5da4c2c03cf939e972fab63e42180da0dba0cf3f
Murat Ayfer <murat@ayfer.net>
2015-03-23 11:56:11 -0700

    basic login functionality

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,42 @@
 # Homepage (Root path)
 get '/' do
+  @message = params[:message]
+  @users = User.all
+  @current_user = request.cookies["username"]
   erb :index
+end
+
+get '/login' do
+  erb :login
+end
+
+post '/login' do
+  params[:username]
+  params[:password]
+
+  users = User.where(username: params[:username]).where(password: params[:password])
+  if users.count > 0
+    user = users[0]
+    response.set_cookie("username", :value => params[:username], :path => "/", :expires => Time.now + 60*60*24*365*3)
+    redirect '/'
+  else
+    # login failed
+    redirect '/?message=Failed'
+  end
+end
+
+get '/signup' do
+  erb :signup
+end
+
+post '/signup' do
+    User.create(username: params[:username], password: params[:password])
+    redirect '/'
+end
+
+get '/logout' do
+    
+    response.set_cookie("username", :value => '', :path => "/", :expires => Time.now - 60*60*24*365*3)
+
+    redirect '/'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140321144528) do
+ActiveRecord::Schema.define(version: 20150323170913) do
 
   create_table "users", force: true do |t|
-    t.string "name"
-    t.string "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "username"
+    t.string   "password"
   end
 
 end
